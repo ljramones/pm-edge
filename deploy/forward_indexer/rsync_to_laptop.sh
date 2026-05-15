910c9fb3c328e0e244a683731aed041024a9e4ce
Larry Mitchell <118214035+lmitchell137@users.noreply.github.com>
2026-05-15 17:48:08 -0400

    added updates to rsync

--- deploy/forward_indexer/rsync_to_laptop.sh
+++ deploy/forward_indexer/rsync_to_laptop.sh
@@ -6,6 +6,6 @@ REMOTE_DIR="${PM_EDGE_REMOTE_FORWARD_INDEX_DIR:-/opt/pm-edge/data/raw/forward_in
 LOCAL_DIR="${PM_EDGE_LOCAL_FORWARD_INDEX_DIR:-data/raw/forward_index/}"
 
 mkdir -p "${LOCAL_DIR}"
-rsync -avz --partial \
+rsync -avz --partial -e "ssh -i ~/.ssh/pm_edge_rsync" \
   "${VPS_HOST}:${REMOTE_DIR}" \
   "${LOCAL_DIR}"
